e8614dc2ea5d1080fd1cbc74ff33b440cf3cb9da
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-02-05 16:45:52 -0300

    Segundo modelo do ReactRouterDom

--- src/components/GetAxios.jsx
+++ src/components/GetAxios.jsx
@@ -1,9 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Tarefa from "./Tarefa";
+import NovaTarefa from "./NovaTarefa"
+import DeleteAxios from "./DeleteAxios"
+import PutAxios from "./PutAxios";
 function GetAxios() {
   const [tarefas, setTarefas] = useState([])
-  const [ref,setRef]=useState(0)
+  const [ref, setRef] = useState(0)
+  function ordenar() {
+    axios
+      .get('https://servidordenotas-5hrw.onrender.com/notas')
+      .then((response) => {
+        console.log(response.data[0].titulo)
+        const ordemAlf = [...response.data]
+        ordemAlf.sort((a, b) => {
+          return a.titulo.localeCompare(b.titulo)
+        })
+        setTarefas(ordemAlf)
+      })
+      .catch((error) => { console.error(error) })
+  }
+
   useEffect(() => {
     axios
       .get('https://servidordenotas-5hrw.onrender.com/notas')
@@ -13,13 +30,18 @@ function GetAxios() {
       })
       .catch((error) => { console.error(error) })
   }, [ref])
+
   return (
     <div>
-      <button onClick={()=>setRef(ref+1)}>Atualizar</button>
+      <h2>Tarefas</h2>
+      <NovaTarefa />
+      <DeleteAxios/>
+      <PutAxios/>
+      <button onClick={() => ordenar()} style={{ margin: 10, borderRadius: 10, padding: 4, backgroundColor: "#1e82f5", fontWeight: 'bold', color: '#FFFFFF', fontSize: 13 }}>Ordem Alfabética</button>
+      <button onClick={() => setRef(ref + 1)} style={{ margin: 10, borderRadius: 10, padding: 4, backgroundColor: "#1e82f5", fontWeight: 'bold', color: '#FFFFFF', fontSize: 13 }}>Ordenar por Id</button>
+      <button onClick={() => setRef(ref + 1)} style={{ margin: 10, borderRadius: 10, padding: 4, backgroundColor: "#1e82f5", fontWeight: 'bold', color: '#FFFFFF', fontSize: 13 }}>Atualizar</button>
       {tarefas.map((element) => (
-        <>
         <Tarefa idTarefa={element.id} tituloTarefa={element.titulo} key={element.id} />
-      </>
       ))}
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import './index.css'
+import ErrorPage from './pages/ErrorPage';
+import GetAxios from './components/GetAxios';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <h3>Página Home</h3>,
+      },
+      {
+        path: "/contato",
+        element: <h3>Página de Contato</h3>,
+      },
+      {
+        path: "/tarefas",
+        element:<GetAxios/>,
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
